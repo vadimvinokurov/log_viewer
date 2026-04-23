916e93853faefc0a8af59f32a56b2ceb35d8fe1e
vvinokurov <vad9fly@gmail.com>
2026-04-24 01:03:41 +0300

    chore: remove SearchToolbar and FilterToolbar imports from views package

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,19 +1,17 @@
 """Views package."""
 from src.views.category_panel import CategoryPanel
-from src.views.filter_toolbar import FilterToolbar
-from src.views.statistics_panel import StatisticsPanel
-from src.views.find_dialog import FindDialog
-
-# Main views
-from src.views.main_window import MainWindow
-from src.views.log_table_view import LogTableView, LogTableModel, LogEntry, LogEntryDisplay
-
-# Widgets
-from src.views.widgets.search_toolbar import SearchToolbar, SearchInput, SearchToolbarWithStats
-from src.views.widgets.statistics_bar import StatisticsBar
 
 # Components
 from src.views.components import CounterWidget
+from src.views.find_dialog import FindDialog
+from src.views.log_table_view import LogEntry, LogEntryDisplay, LogTableModel, LogTableView
+
+# Main views
+from src.views.main_window import MainWindow
+from src.views.statistics_panel import StatisticsPanel
+
+# Widgets
+from src.views.widgets.statistics_bar import StatisticsBar
 
 __all__ = [
     # Main classes
@@ -22,17 +20,13 @@ __all__ = [
     "LogTableModel",
     "LogEntry",
     "LogEntryDisplay",
-    
+
     # Widgets
-    "SearchToolbar",
-    "SearchInput",
-    "SearchToolbarWithStats",
     "StatisticsBar",
     "CounterWidget",
-    
+
     # Other views
     "CategoryPanel",
-    "FilterToolbar",
     "StatisticsPanel",
     "FindDialog",
 ]
